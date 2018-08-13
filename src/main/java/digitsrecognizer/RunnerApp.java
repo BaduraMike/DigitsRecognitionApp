b16b22609e055deaabe4390692eb0435b6ddcc04
Michal Badura <badura.mike@gmail.com>
2018-08-13 13:53:02 +0200

    Splitting of rows from previously created List to columns.

--- src/main/java/digitsrecognizer/RunnerApp.java
+++ src/main/java/digitsrecognizer/RunnerApp.java
@@ -5,22 +5,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RunnerApp {
 
     public static void main(String[] args) throws IOException {
-
         Path path = Paths.get("C:\\Users\\badur\\Desktop\\SDA\\Programowanie II\\DigitsRecognitionApp",
                 "trainingsample.csv");
 
         List<String> readAllLines;
 
-        try{
+        try {
             readAllLines = Files.readAllLines(path);
-        }catch (IOException ex){
+        } catch (IOException ex) {
             throw new IOException("File not found");
         }
 
+        List<String[]> listOfStringLists = readAllLines
+                .subList(0, readAllLines.size())
+                .stream()
+                .map(x->x.split(","))
+                .collect(Collectors.toList());
     }
 
 }
